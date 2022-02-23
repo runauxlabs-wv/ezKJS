355e0cdeb2106d858d448c161154d3829280e0d1
MakeWayForMe <neojs89@gmail.com>
2022-02-23 10:46:12 +0900

    220223 공통js 정리

--- pb/js/index.js
+++ pb/js/index.js
@@ -1,16 +1,5 @@
 window.onload = function() {
-    //브라우저 크기조절 리프레시
-    var lastWidth = window.innerWidth;
-
-    function changeWinRefresh() {
-        if (window.innerWidth != lastWidth) {
-            window.location = window.location;
-            lastWidth = window.innerWidth;
-            return false;
-        }
-    }
-    window.addEventListener('resize', changeWinRefresh);
-
+    
     //배경슬라이드
     $('.background > img:gt(0)').hide();
     setInterval(function() {
@@ -43,38 +32,6 @@ window.onload = function() {
         },
     });
 
-    //nav
-    $('.sec1top > .inner').on('mouseenter focusin', function() {
-        $(".sec1top").addClass('on');
-    }).on('mouseleave focusout', function() {
-        $(".sec1top").removeClass('on');
-    });
-    
-    $('.menuicon').click(function() {
-        if($('.menuicon').hasClass('fa-bars')) {
-            $('.menu, .member div').addClass('on');
-            $(this).removeClass('fa-bars').addClass('fa-times').css('color', '#005BAB');
-        } else {
-            $('.menu, .member div').removeClass('on');
-            $(this).removeClass('fa-times').addClass('fa-bars').css('color', 'white');
-        }
-    });
-    
-    var iw = innerWidth;
-    if(iw > 1199) {
-        $('.gnb > li').on('mouseenter focusin', function() {
-            $(this).children('.snb').addClass('on');
-        }).on('mouseleave focusout', function() {
-            $(this).children('.snb').removeClass('on');
-        });
-    } else {
-        $('.snb').hide();
-        $('.gnb > li > a').click(function(event) {
-            $(this).next().stop(true).slideToggle();
-            $('.gnb > li > a').not(this).next(false,true).slideUp();
-            return false
-        });
-    };
     //책들
     var swiper = new Swiper(".contents", {
         direction: "vertical",
--- pb/js/ogrcht.js
+++ pb/js/ogrcht.js
@@ -10,40 +10,6 @@ window.onload = function() {
         }
     }
     window.addEventListener('resize', changeWinRefresh);
-    
-    //nav
-    $('.sec1top > .inner').mouseenter(function() {
-        $(".sec1top").addClass('on');
-    }).mouseleave(function() {
-        $(".sec1top").removeClass('on');
-    });
-    
-    $('.menuicon').click(function() {
-        if($('.menuicon').hasClass('fa-bars')) {
-            $('.menu, .member div').addClass('on');
-            $(this).removeClass('fa-bars').addClass('fa-times').css('color', '#005BAB');
-        } else {
-            $('.menu, .member div').removeClass('on');
-            $(this).removeClass('fa-times').addClass('fa-bars').css('color', 'white');
-        }
-    });
-    
-    var iw = innerWidth;
-    if(iw > 1199) {
-        $('.gnb > li').on('mouseenter focusin', function() {
-            $(this).children('.snb').addClass('on');
-        }).on('mouseleave focusout', function() {
-            $(this).children('.snb').removeClass('on');
-        });
-    } else {
-        $('.snb').hide();
-        $('.gnb > li > a').click(function(event) {
-            $(this).next().stop(true).slideToggle();
-            $('.gnb > li > a').not(this).next().stop(false,true).slideUp();
-            return false
-        });
-        $('p').has('.fa-print').hide(); //프린트 숨김
-    };
 
     //조직도 이벤트
     $.ajax({
